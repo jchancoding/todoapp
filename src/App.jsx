987dcb5a30ffd429f31bc3c596c1476f3b5ecf6b
Jasper Chan <32831824+jchancoding@users.noreply.github.com>
2024-08-27 15:53:26 -0700

    Add Header
    Added Header component and styling
    Removed Outline and Inline component and styling
    Added CSS sheet to App.jsx to remove margins

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import Todo from "./components/Todo";
-import InlineComponent from "./components/InlineComponent";
-import OutlineComponent from "./components/OutlineComponent";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Todo />
-      <InlineComponent />
-      <OutlineComponent />
     </div>
   );
 }
